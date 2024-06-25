204bf227750a6df3dc186b6acb66f13ffb18d6b8
R4r3sh1k4 <stanciu.r.25022004@gmail.com>
2024-06-25 14:20:28 +0300

    Intro Borders :3

--- Project_Obsidian_3/Intro.cpp
+++ Project_Obsidian_3/Intro.cpp
@@ -46,43 +46,44 @@ void Text_Intro_Narration() {
 }
 
 void Text_Intro() {
-    std::string Intro_Text_l1= " The Crimson Imperium stands as a towering colossus of oppression and conquest, casting its dark shadow across the lands it seeks to subjugate.\n";
 
-    std::string Intro_Text_l2 = "From the blood-stained halls of its citadels to the scorched fields of its conquered territories, the Imperium's presence is felt like a relentless \n";
-        std::string Intro_Text_l2_2 = " storm of death and despair.\n ";
+ std::string Intro_Text_l1 = " The Crimson Imperium stands as a towering colossus of oppression and conquest, casting its dark shadow across the lands it seeks to subjugate. ";
 
-    std::string Intro_Text_l3 = "At the helm of this malevolent empire are the Sovereigns, also called The Crimson Hand, ruthless despots whose thirst for power knows no bounds.\n ";
+ std::string Intro_Text_l2 = "From the blood-stained halls of its citadels to the scorched fields of its conquered territories, the Imperium's presence is felt like a relentless ";
+ std::string Intro_Text_l2_2 = " storm of death and despair. ";
 
-    std::string Intro_Text_l4 = "They rule with an iron fist, their decrees enforced by legions of merciless clone soldiers and elite special forces who strike fear into the hearts\n";
-        std::string Intro_Text_l4_2 = "of even the most defiant rebels.\n ";
+ std::string Intro_Text_l3 = "At the helm of this malevolent empire are the Sovereigns, also called The Crimson Hand, ruthless despots whose thirst for power knows no bounds. ";
 
-    std::string Intro_Text_l5 = "In the wake of the Imperium's relentless march, cities lie in ruins and once-thriving civilizations are reduced to ashes. \n ";
+ std::string Intro_Text_l4 = "They rule with an iron fist, their decrees enforced by legions of merciless clone soldiers and elite special forces who strike fear into the hearts ";
+ std::string Intro_Text_l4_2 = "of even the most defiant rebels. ";
 
-    std::string Intro_Text_l6 = "The cries of the oppressed echo through the streets, drowned out only by the deafening roar of war machines and the clash of steel on steel.\n ";
+ std::string Intro_Text_l5 = "In the wake of the Imperium's relentless march, cities lie in ruins and once-thriving civilizations are reduced to ashes. ";
 
-    std::string Intro_Text_l7 = "At the forefront of the Imperium's relentless expansion are its legions of cloned warriors, bred in the depths of shadowy laboratories and raised\n";
-        std::string Intro_Text_l7_2 ="for one purpose alone : to sow destruction and death upon the battlefield.\n ";
+ std::string Intro_Text_l6 = "The cries of the oppressed echo through the streets, drowned out only by the deafening roar of war machines and the clash of steel on steel. ";
 
-    std::string Intro_Text_l8 = "These clones, stripped of individuality and autonomy, are nothing more than disposable tools of war, their lives measured not in years but in \n";
-        std::string Intro_Text_l8_2 = "battles fought and enemies slain.\n ";
+ std::string Intro_Text_l7 = "At the forefront of the Imperium's relentless expansion are its legions of cloned warriors, bred in the depths of shadowy laboratories and raised ";
+ std::string Intro_Text_l7_2 ="for one purpose alone : to sow destruction and death upon the battlefield. ";
 
-    std::string Intro_Text_l9 = "Promised the illusion of advancement and power, these cloned soldiers march willingly to their doom, their minds clouded by false promises and twisted propaganda.\n ";
+ std::string Intro_Text_l8 = "These clones, stripped of individuality and autonomy, are nothing more than disposable tools of war, their lives measured not in years but in ";
+ std::string Intro_Text_l8_2 = "battles fought and enemies slain. ";
 
-    std::string Intro_Text_l10 = "They are told that glory and prestige await those who prove themselves on the field of battle and that they will rise through the ranks\n";
-        std::string Intro_Text_l10_2 = " to positions of authority and influence within the empire. \n ";
+ std::string Intro_Text_l9 = "Promised the illusion of advancement and power, these cloned soldiers march willingly to their doom, their minds clouded by false promises and twisted propaganda. ";
 
-    std::string Intro_Text_l11 = "Yet, the reality is far more grim.The vast majority of clones never live to see their promised promotions, their lives cut short\n";
-        std::string Intro_Text_l11_2= " by the relentless grind of war as its their purpose.\n ";
+ std::string Intro_Text_l10 = "They are told that glory and prestige await those who prove themselves on the field of battle and that they will rise through the ranks ";
+ std::string Intro_Text_l10_2 = " to positions of authority and influence within the empire. ";
 
-    std::string Intro_Text_l12 = "Even those who manage to claw their way to the upper echelons of the Imperium's hierarchy find themselves ensnared in a web of deceit \n";
-        std::string Intro_Text_l12_2 = " and manipulation, their every move watched and scrutinized by those who would see them fall.\n ";
+ std::string Intro_Text_l11 = "Yet, the reality is far more grim.The vast majority of clones never live to see their promised promotions, their lives cut short ";
+ std::string Intro_Text_l11_2= " by the relentless grind of war as its their purpose. ";
 
-    std::string Intro_Text_l13 = "Power is fleeting, and those who grasp for it too eagerly often find themselves consumed by the very darkness they sought to conquer. \n ";
+ std::string Intro_Text_l12 = "Even those who manage to claw their way to the upper echelons of the Imperium's hierarchy find themselves ensnared in a web of deceit ";
+ std::string Intro_Text_l12_2 = " and manipulation, their every move watched and scrutinized by those who would see them fall. ";
 
-    std::string Intro_Text_l14 = "In the end, the Crimson Imperium is a realm where hope is but a distant memory, and the promise of advancement is nothing more than a cruel joke. \n ";
+ std::string Intro_Text_l13 = "Power is fleeting, and those who grasp for it too eagerly often find themselves consumed by the very darkness they sought to conquer. ";
 
-    std::string Intro_Text_l15 = "For in a world built upon the foundations of suffering and death, true power comes not from titles or accolades, but from the willingness to\n";
-        std::string Intro_Text_l15_2 = " embrace the darkness within oneself and to cast aside all semblance of humanity in the pursuit of absolute domination. \n";
+ std::string Intro_Text_l14 = "In the end, the Crimson Imperium is a realm where hope is but a distant memory, and the promise of advancement is nothing more than a cruel joke. ";
+
+ std::string Intro_Text_l15 = "For in a world built upon the foundations of suffering and death, true power comes not from titles or accolades, but from the willingness to ";
+ std::string Intro_Text_l15_2 = " embrace the darkness within oneself and to cast aside all semblance of humanity in the pursuit of absolute domination. ";
 
 
 
@@ -90,12 +91,13 @@ void Text_Intro() {
     std::vector<std::string> Text_Intro;
     ///////////////////////////////////////////////////////////// Places Line 1
     Text_Intro.push_back(Intro_Text_l1);
-
+    T_Border(100);
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
-    
+    R_Border();
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
     // 
@@ -103,11 +105,12 @@ void Text_Intro() {
     Text_Intro.push_back(Intro_Text_l2);
     std::this_thread::sleep_for(std::chrono::seconds(2));
     
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -115,11 +118,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 2-2
     Text_Intro.push_back(Intro_Text_l2_2);
     std::this_thread::sleep_for(std::chrono::seconds(1));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -127,11 +131,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 3
     Text_Intro.push_back(Intro_Text_l3);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -139,11 +144,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 4
     Text_Intro.push_back(Intro_Text_l4);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -151,11 +157,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 4-2
     Text_Intro.push_back(Intro_Text_l4_2);
     std::this_thread::sleep_for(std::chrono::seconds(1));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -163,11 +170,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 5
     Text_Intro.push_back(Intro_Text_l5);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -175,11 +183,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 6
     Text_Intro.push_back(Intro_Text_l6);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -187,11 +196,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 7
     Text_Intro.push_back(Intro_Text_l7);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -199,11 +209,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 7-2
     Text_Intro.push_back(Intro_Text_l7_2);
     std::this_thread::sleep_for(std::chrono::seconds(1));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -211,11 +222,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 8
     Text_Intro.push_back(Intro_Text_l8);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -223,11 +235,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 8-2
     Text_Intro.push_back(Intro_Text_l8_2);
     std::this_thread::sleep_for(std::chrono::seconds(1));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -235,11 +248,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 9
     Text_Intro.push_back(Intro_Text_l9);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -247,11 +261,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 10
     Text_Intro.push_back(Intro_Text_l10);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -259,11 +274,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 10-2
     Text_Intro.push_back(Intro_Text_l10_2);
     std::this_thread::sleep_for(std::chrono::seconds(1));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -271,11 +287,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 11
     Text_Intro.push_back(Intro_Text_l11);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -283,10 +300,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 11-2
     Text_Intro.push_back(Intro_Text_l11_2);
     std::this_thread::sleep_for(std::chrono::seconds(1)); 
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -294,10 +313,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 12
     Text_Intro.push_back(Intro_Text_l12);
     std::this_thread::sleep_for(std::chrono::seconds(2)); 
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -305,10 +326,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 12-2
     Text_Intro.push_back(Intro_Text_l12_2);
     std::this_thread::sleep_for(std::chrono::seconds(1)); 
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -316,10 +339,12 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 13
     Text_Intro.push_back(Intro_Text_l13);
     std::this_thread::sleep_for(std::chrono::seconds(2)); 
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -327,21 +352,25 @@ void Text_Intro() {
     ///////////////////////////////////////////////////////////// Places Line 14
     Text_Intro.push_back(Intro_Text_l14);
     std::this_thread::sleep_for(std::chrono::seconds(2));
-
+    L_Border();
     for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
     // 
     ///////////////////////////////////////////////////////////// Places Line 15
     Text_Intro.push_back(Intro_Text_l15);
-    std::this_thread::sleep_for(std::chrono::seconds(2)); for (char C : Text_Intro[0]) {
+    std::this_thread::sleep_for(std::chrono::seconds(2)); 
+    L_Border();
+    for (char C : Text_Intro[0]) {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector
@@ -354,6 +383,7 @@ void Text_Intro() {
         std::cout << C;
         std::this_thread::sleep_for(std::chrono::milliseconds(45));
     }
+    R_Border();
 
     Text_Intro.clear();
     //////////////////////////////////////////////////////////// Clears the vector for the last time :3
--- Project_Obsidian_3/Intro.h
+++ Project_Obsidian_3/Intro.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "Master_Header.h"
-
+#include "Misc_Functions.h"`
 void Text_Intro_Narration();
 void Text_Intro();
 void Intro_audio();
